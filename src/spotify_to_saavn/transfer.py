ad0a1c0b539c0fe6d7f415a40eb831eef26da29b
Neha Kantheti <nehakantheti@gmail.com>
2026-02-25 15:47:38 +0530

    Added additional functions to wire required methods for transfer

--- src/spotify_to_saavn/transfer.py
+++ src/spotify_to_saavn/transfer.py
@@ -42,3 +42,30 @@ class TransferManager:
 
         logger.info(f"Matching complete. Found {len(matched_saavn_ids)} tracks. Missed {len(missing_tracks)} tracks.")
         return matched_saavn_ids, missing_tracks
+    
+    def execute_transfer(self, spotify_playlist_id: str, new_playlist_name: str) -> bool:
+        """Return True if the operation is successful else false"""
+        logger.info(f'Starting full transfer for spotify playlist {spotify_playlist_id} to Saavn with playlist name {new_playlist_name}')
+
+        matched_ids, missing_tracks = self.get_jiosaavn_track_ids(spotify_playlist_id)
+
+        if not matched_ids:
+            logger.error(f"No tracks were matched on Saavn. Aborting transfer")
+            return False
+        
+        new_playlist_id = self.saavn.create_playlist(new_playlist_name)
+        if not new_playlist_id:
+            logger.error(f"Error encountered while creating a new playlist with name {new_playlist_name}")
+            return False
+        
+        # ADd only matched ids to the new playlist created
+        success = self.saavn.add_songs_to_playlist(new_playlist_id, matched_ids)
+
+        if success:
+            logger.info(f"Transfer from spotify playlist {spotify_playlist_id} to new playlist with id {new_playlist_id} on Saavn is successful!\n")
+            if missing_tracks:
+                logger.warning(f'{len(missing_tracks)} tracks are missing on saavn in the playlist {new_playlist_id} - Not added to the saavn playlist with id {new_playlist_id}')
+                return True
+        else:
+            logger.error(f'Failed to add songs to the newly created playlist on saavn.')
+            return False
--- tests/test_transfer.py
+++ tests/test_transfer.py
@@ -30,3 +30,31 @@ def test_transfer_playlist_workflow(mocker):
 
     mock_spotify.get_playlist_tracks.assert_called_once_with('fake_playlist_id_123')
     assert mock_saavn.search_song.call_count == 2
+
+def test_execute_transfer(mocker):
+    mock_spotify = mocker.MagicMock()
+    mock_saavn = mocker.MagicMock()
+
+    mock_saavn.create_playlist.return_value = 'P999'
+    mock_saavn.add_songs_to_playlists.return_value = True
+
+    manager = TransferManager(spotify_client=mock_spotify, saavn_client=mock_saavn)
+
+    # Mocking TransferManager's internal method since it is already tested separately
+    mocker.patch.object(
+        manager,
+        'get_jiosaavn_track_ids',
+        # matched song ids and missing tracks
+        return_value=(['S123', 'S456'], [{'name': 'Missing Song'}])
+    )
+
+    result = manager.execute_transfer(
+        spotify_playlist_id="fake_spotify_id_123",
+        new_playlist_name="My Awesome Summer Mix"
+    )
+
+    assert result is True
+
+    manager.get_jiosaavn_track_ids.assert_called_once_with('fake_spotify_id_123')
+    mock_saavn.create_playlist.assert_called_once_with("My Awesome Summer Mix")
+    mock_saavn.add_songs_to_playlist.assert_called_once_with("P999", ['S123', 'S456'])
